9bb008be406231b2e7082b59995351d153368d58
Kseniia <ksusha.yurieva@gmail.com>
2019-12-05 22:03:09 +0200

    Changed the HTML for the site.

---
+++ comments/templates/comments/js/star.js
@@ -0,0 +1,25 @@
+document.querySelector("#star-1").onclick = function(){
+		document.querySelector("#block2").style.display = "none";
+        document.querySelector("#block1").style.display = "block";
+
+}
+document.querySelector("#star-2").onclick = function(){
+		document.querySelector("#block2").style.display = "none";
+        document.querySelector("#block1").style.display = "block";
+
+}
+document.querySelector("#star-3").onclick = function(){
+		document.querySelector("#block2").style.display = "none";
+        document.querySelector("#block1").style.display = "block";
+
+}
+document.querySelector("#star-4").onclick = function(){
+        document.querySelector("#block1").style.display = "none";
+        document.querySelector("#block2").style.display = "block";
+
+}
+document.querySelector("#star-5").onclick = function(){
+        document.querySelector("#block1").style.display = "none";
+        document.querySelector("#block2").style.display = "block";
+
+}
