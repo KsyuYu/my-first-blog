2b1375c5e1e6327cc393e772cc7d2e80a97743e5
Kseniia <ksusha.yurieva@gmail.com>
2019-12-10 19:30:26 +0200

    Added views to create/edit blog post inside the site.

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from .models import Comment
 # Create your views here.
 def post_list(request):
     return render(request, 'comments/post_list.html', {})
